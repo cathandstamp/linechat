84fa3cc0dca80ebd13d4176aa92e6b785ca36b1d
猫のて <72399484+cathandstamp@users.noreply.github.com>
2021-03-09 04:25:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,40 @@ const express = require('express');
 const app = express();
 const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 5000
-
 const { Client } = require('pg');
-
-const connection = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-   });
-   connection.connect();
-
-   const create_userTable = {
-    text:'CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, line_uid VARCHAR(255), display_name VARCHAR(255), timestamp VARCHAR(255), cuttime SMALLINT, shampootime SMALLINT, colortime SMALLINT, spatime SMALLINT);'
- };
-
 
 
 const config = {
-   channelAccessToken:process.env.ACCESS_TOKEN,
-   channelSecret:process.env.CHANNEL_SECRET
+  channelAccessToken:process.env.ACCESS_TOKEN,
+  channelSecret:process.env.CHANNEL_SECRET
 };
 
 const client = new line.Client(config);
+
+const connection = new Client({
+  user:process.env.PG_USER,
+  host:process.env.PG_HOST,
+  database:process.env.PG_DATABASE,
+  password:process.env.PG_PASSWORD,
+  port:5433
+});
+
+
+const create_userTable = {
+  text:'CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, line_uid VARCHAR(255), display_name VARCHAR(255), timestamp VARCHAR(255), cuttime SMALLINT, shampootime SMALLINT, colortime SMALLINT, spatime SMALLINT);'
+};
+
+connection.query(create_userTable) .then(()=>{ 
+  console.log('table users created successfully!!'); 
+}) .catch(e=>console.log(e));
+
+
 
 app
    .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
    .listen(PORT,()=>console.log(`Listening on ${PORT}`));
 
-   const lineBot = (req,res) => {
+    const lineBot = (req,res) => {
       res.status(200).end();
       const events = req.body.events;
       const promises = [];
@@ -51,21 +56,13 @@ app
           .then(console.log('all promises passed'))
           .catch(e=>console.error(e.stack));
    }
+
+
    const greeting_follow = async (ev) => {
       const profile = await client.getProfile(ev.source.userId);
       return client.replyMessage(ev.replyToken,{
           "type":"text",
           "text":`${profile.displayName}さん、フォローありがとうございます\uDBC0\uDC04`
-          const table_insert = {
-            text:'INSERT INTO users (line_uid,display_name,timestamp,cuttime,shampootime,colortime,spatime) VALUES($1,$2,$3,$4,$5,$6,$7);',
-            values:[ev.source.userId,profile.displayName,ev.timestamp,INITIAL_TREAT[0],INITIAL_TREAT[1],INITIAL_TREAT[2],INITIAL_TREAT[3]]
-          }
-          connection.query(table_insert)
-            .then(()=>{
-               console.log('insert successfully!!')
-             })
-            .catch(e=>console.log(e));
-    
         });
 
     }
